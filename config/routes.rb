9ab234959f6ff700a749a31e4df6fc36f28a4242
Yamal Soueidan <jamal@soueidan.com>
2012-12-05 22:33:12 +0100

    changed routes to debta

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 # -*- encoding : utf-8 -*-
-
 VandpibeCafe::Application.routes.draw do
   ActiveAdmin.routes(self)
 
@@ -16,19 +15,19 @@ VandpibeCafe::Application.routes.draw do
   resources :locations, :only => [:create, :destroy]
 
   match '/get_json' => "main#get_json", :via => :get
-  match '/spørgsmål' => "questions#index", :as => 'questions', :via => :get
-  match '/sp%C3%B8rgsm%C3%A5l' => "questions#create", :as => 'questions', :via => :post
+  match '/debat' => "questions#index", :as => 'questions', :via => :get
+  match '/debat' => "questions#create", :as => 'questions', :via => :post
 
-  match '/sp%C3%B8rgsm%C3%A5l/:id/update' => "questions#update",  :as => 'update_question', :via => :put
-  match '/sp%C3%B8rgsm%C3%A5l/:id/delete' => "questions#destroy", :as => 'delete_question', :via => :delete
-  match '/sp%C3%B8rgsm%C3%A5l/:id/edit' => "questions#edit", :as => 'edit_question', :via => :get
+  match '/debat/:id/update' => "questions#update",  :as => 'update_question', :via => :put
+  match '/debat/:id/delete' => "questions#destroy", :as => 'delete_question', :via => :delete
+  match '/debat/:id/edit' => "questions#edit", :as => 'edit_question', :via => :get
   
-  match '/sp%C3%B8rgsm%C3%A5l/ubesvaret' => "questions#unanswered", :as => 'unanswered_questions', :via => :get
-  match '/sp%C3%B8rgsm%C3%A5l/mest' => "questions#hot", :as => 'hot_questions', :via => :get
-  match '/sp%C3%B8rgsm%C3%A5l/seneste' => "questions#recent", :as => 'recent_questions', :via => :get
-  match '/sp%C3%B8rgsm%C3%A5l/opret' => "questions#new", :as => 'new_question', :via => :get
+  match '/debat/ubesvaret' => "questions#unanswered", :as => 'unanswered_questions', :via => :get
+  match '/debat/mest' => "questions#hot", :as => 'hot_questions', :via => :get
+  match '/debat/seneste' => "questions#recent", :as => 'recent_questions', :via => :get
+  match '/debat/opret' => "questions#new", :as => 'new_question', :via => :get
 
-  match '/sp%C3%B8rgsm%C3%A5l/:id/*title' => "questions#show",    :as => 'question', :via => :get
+  match '/debat/:id/*title' => "questions#show",    :as => 'question', :via => :get
   
   match '/:name' => "cities#show", :as => 'city'
   match '/:city_name/:name' => "locations#show", :as => 'city_location'
