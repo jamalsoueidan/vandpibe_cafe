be86abe72291aceefebec63eb2a238bdf1b0098e
Yamal Soueidan <jamal@soueidan.com>
2012-12-14 10:18:38 +0100

    fixed facebook api code, and added question to admin panel

--- config/initializers/omni_auth.rb
+++ config/initializers/omni_auth.rb
@@ -1,4 +1,4 @@
 # -*- encoding : utf-8 -*-
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '148263508630116', '495ddfd26e18692f46a2a9313e38ad7f'
+  provider :facebook, "275375345884782", "d55297abfb19cfc4ed3d2cf87bd684d8"
 end
