190d6fb4d336ced707c32e4e2812072e6fc832e4
Yamal Soueidan <jamal@soueidan.com>
2012-12-20 06:22:32 +0100

    Updated footer

---
+++ lib/find_by_url.rb
@@ -0,0 +1,41 @@
+# -*- encoding : utf-8 -*-
+class String
+  def utf8!
+    self.gsub!('aa', 'å')
+    self.gsub!('oe', 'ø')
+    self.gsub!('ae', 'æ')
+    self.gsub!('+', ' ')
+    self.gsub!('-', ' ')
+  end
+  
+  def utf8
+    text = self
+    text.gsub!('aa', 'å')
+    text.gsub!('oe', 'ø')
+    text.gsub!('ae', 'æ')
+    text.gsub!('+', ' ')
+    text.gsub!('-', ' ')
+    return text
+  end
+  
+  def big
+    string = self.titleize
+    string[0] = string[0].gsub('å', 'Å')
+    string[0] = string[0].gsub('ø', 'Ø')
+    string[0] = string[0].gsub('æ', 'Æ')
+    return string
+  end
+end
+
+module FindByURL
+  extend ActiveSupport::Concern
+  
+  module ClassMethods
+    def find_by_url(name)
+      name.downcase!
+      self.where("#{self.table_name}.name = ? OR #{self.table_name}.name = ?", name, name.utf8).first
+    end
+  end
+end
+
+ActiveRecord::Base.send(:include, FindByURL)
