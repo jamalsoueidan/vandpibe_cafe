86cb1d3d2273647566a389487702df4f817ae36e
Jamal Soueidan <jamal@soueidan.com>
2013-04-09 11:34:20 +0200

    Updated admin section to delete many comments at once

--- app/admin/city.rb
+++ app/admin/city.rb
@@ -1,3 +1,11 @@
 ActiveAdmin.register City do
-  
+  index do	
+  	column :name
+  	column :color do |city|
+  		div :style => "background-color:#" + city.color do
+  			city.color
+  		end
+  	end
+  	default_actions
+  end
 end
--- app/admin/comments.rb
+++ app/admin/comments.rb
@@ -1,3 +1,12 @@
 ActiveAdmin.register Comment do
-  
+  index do      
+	selectable_column
+	column :location do |comment|
+		div do
+			comment.location.name
+		end
+	end
+    column :body
+    default_actions
+  end
 end
