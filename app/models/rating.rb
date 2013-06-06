2bcfdf4cf388983d082381abd464f10b7c420642
Jamal Soueidan <jamal@soueidan.com>
2013-06-06 17:55:23 +0200

    Forgot to add user_id to attr_accessible

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,7 +1,7 @@
 class Rating < ActiveRecord::Base
   KEYS = ['vandpibe', 'service', 'indretning', 'social', ]
   
-  attr_accessible :rating_key, :rating_value
+  attr_accessible :rating_key, :rating_value, :user_id
 
   belongs_to :user
   belongs_to :location
