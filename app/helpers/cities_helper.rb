b4acba7aa43ffb8efe668b04b518b10e96fd594c
Jamal Soueidan <jamal@soueidan.com>
2013-06-09 21:23:19 +0200

    Write something when location description is missing on show location under city controller

--- app/helpers/cities_helper.rb
+++ app/helpers/cities_helper.rb
@@ -1,2 +1,9 @@
 module CitiesHelper
+	def description(location)
+		if location.description.nil? || location.description == ""
+			return 'Denne Vandpibe Cafe mangler beskrivelse.'
+		else
+			return truncate(location.description, :length => 80)
+		end
+	end
 end
