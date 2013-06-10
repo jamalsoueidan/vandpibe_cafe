ea6ff192861c39298bf8dfc7b9a2f4df2cf3301c
Jamal Soueidan <jamal@soueidan.com>
2013-06-10 23:04:22 +0200

    Cleaned City index file and refactored css web files so we can use the buildin include in the foundation zurb

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -5,7 +5,7 @@ class CitiesController < ApplicationController
 
   def index
     @locations = Location.where(:visible => true).order('RAND()').limit(12)
-    @users = User.order('RAND()').limit(15)
+    #@users = User.order('RAND()').select('avatar_url').limit(25)
   end
 
   def old_url
