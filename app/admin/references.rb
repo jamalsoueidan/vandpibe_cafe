4634a5925b53d16dfbb0f8e8a0bd6aa3c792dd93
Jamal Soueidan <jamal@soueidan.com>
2013-11-16 15:12:24 +0100

    Add permitted params for reference

--- app/admin/references.rb
+++ app/admin/references.rb
@@ -1,3 +1,9 @@
 ActiveAdmin.register Reference do
   menu :parent => "Countries"
+
+  controller do
+    def permitted_params
+      params.permit(:reference => [:location_id, :name, :link])
+    end
+  end
 end
